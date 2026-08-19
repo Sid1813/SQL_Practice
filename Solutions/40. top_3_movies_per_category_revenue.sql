22f05e6b3d10131c5f9697e1eb8ca760f7835c99
Sid1813 <sidhu2sharp@gmail.com>
2026-08-20 00:22:39 +0530

    added comments + diff solution

--- Solutions/40. top_3_movies_per_category_revenue.sql
+++ Solutions/40. top_3_movies_per_category_revenue.sql
@@ -22,7 +22,6 @@ where category_rank_by_revenue <= 3
 
 -- Solution for the same problem as above using 2 CTEs
 
-
 with revenue_cte as (select f.film_id, -- This CTE is to find the revenue of all films by coalescing the amount earned by each film across all its rentals
     f.title, 
     f.category,
@@ -33,19 +32,13 @@ group by f.film_id,
     f.title, 
     f.category),
 
-revenue_ranks as ( -- This CTE is to rank all the films by total revenue ( amont + tax) in descending order, by category
-
-select f.film_id, 
-    f.title, 
-    f.category,
-    sum(coalesce(r.amount, 0) + coalesce(r.tax, 0)) as total_film_revenue,
+revenue_ranks as ( select film_id, -- This CTE is to rank all the films by total revenue ( amont + tax) in descending order, by category
+    title, 
+    category,
+    total_film_revenue,
     dense_rank() over (
-partition by f.category order by sum(coalesce(r.amount, 0) + coalesce(r.tax, 0)) desc) as revenue_rank
-from film f
-join rental r on r.film_id = f.film_id
-group by f.film_id, 
-    f.title, 
-    f.category
+partition by category order by total_film_revenue desc) as revenue_rank -- Imortant! You generally cannot referenc an alias within a window function. 
+from revenue_cte                                                        -- But this gets a pass since this alias (total_film_revenue) is from another CTE.
 )
 
 select film_id, -- Final select statement to fimnd top 3 highest revenue films by category
@@ -55,3 +48,4 @@ select film_id, -- Final select statement to fimnd top 3 highest revenue films b
        revenue_rank
 from revenue_ranks
 where revenue_rank <= 3
+
